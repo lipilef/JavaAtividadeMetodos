e482c9cf988ec3de5117fe5dfa6a1221bd4b595f
ramonsl <ramonsl@gmail..com>
2018-04-09 19:08:29 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+        /*
+
+        https://docs.google.com/document/d/1RJjWNLIV-f52q6uo7zVm9bRINsJk-yI8wdWk6DDW5Q0/edit?usp=sharing
+
+
+
+         */
+
+
+
+
+
+            System.out.println(Mes.getMesPorExtenso(1, 1));
+            System.out.println(Mes.getMesPorExtenso(2, 1));
+            System.out.println(Mes.getMesPorExtenso(2, 13));
+            System.out.println(Mes.getMesPorExtenso(12, 5));
+
+        Contagem.contar();
+        Contagem.contar(5);
+        Contagem.contar(8, 18);
+        Contagem.contar(2, 8, 3);
+
+
+
+    }
+}
