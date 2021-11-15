dfd15deed0a31ac0c4fd9d3a6ccef6530b9ac451
Felipe Alves <lipilef@gmail.com>
2021-11-15 16:32:16 -0300

    Update Mes.java
    Update de otimização no código e validações para a classe de teste

--- src/Mes.java
+++ src/Mes.java
@@ -1,39 +1,28 @@
-public class Mes {
+public class Ex_01 {
+    public  static String mes [] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+    public  static String month [] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 
-    public  static   String meses [] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
-    public  static   String months [] = {"January","Febraro","Marc","April","May","June","July","August","September","October","November","December"};
-
-
-    public static String getMes(int m){
-        m=m-1;
-        if(m>=0 && m<12){
-            return meses[m];
+   public static String getMes(int m){
+       m = m - 1;
+       if (m >= 0 && m < 12){
+           return mes[m];
+       } else{
+           return "Mês inválido";
+       }
+   }
+   public static String getMonth(int m){
+        m = m - 1;
+        if (m >= 0 && m < 12){
+            return month[m];
+        } else{
+            return "Invalid Value";
         }
-        else{
-            return "Mês invalido";
-        }
-    }
-
-    public static String getMonths(int m){
-        m=m-1;
-        if(m>=0 && m<12){
-            return months[m];
-        }
-        else{
-            return "Mês invalido";
-        }
-    }
+   }
 
     public static String getMesPorExtenso(int i, int m) {
-        if (i == 1) {
-            return getMes(m);
-        }
-        if (i == 2) {
-            return getMonths(m);
-        }
-
-            return "idioma não suportado";
+        if (i == 1) return getMes(m);
+        if (i == 2) return getMonth(m);
+        return "Idioma Não Suportado";
     }
-
 
 }
